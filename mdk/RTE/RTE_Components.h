6203825a14b882864d1d9b6e16a5631e10fc6d26
Manuel Limones <limonesu.me@gmail.com>
2015-07-08 09:40:50 -0500

    rename keil proyect

--- mdk/RTE/RTE_Components.h
+++ mdk/RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'SPL_04_UART_STM32F3' 
+ * Project: 'SPL_09_I2C_STM32F3' 
  * Target:  'Target 1' 
  */
 
